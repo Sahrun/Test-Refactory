593cef0914bc3a0569d8b234fbda12b220e3df09
Sahrun <sahrun.raja.tega@gmail.com>
2020-12-23 19:09:08 +0700

    Update now.blade.php

--- BookingRoms/resources/views/booking/now.blade.php
+++ BookingRoms/resources/views/booking/now.blade.php
@@ -11,7 +11,9 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="col-lg-6">
+                         @if(isset($viewdata['room_photo']))
                         <img src="{!! Route('storage',['filename'=>$viewdata['room_photo']]) !!}" class="img-fluid" alt="image">
+                        @endif
                         </div>
                         </div>
                         <div class="col-lg-6">
